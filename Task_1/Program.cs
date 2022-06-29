e53e879eff1b507a8e896e3a490a40420ab09c2b
Maria <ma.pechenkina@yandex.ru>
2022-06-29 18:51:27 +0300

    added tasks 1-2

---
+++ Task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+
+void PrintArray(int[] PArray)
+{
+    for (int j = 0; j < PArray.Length; j++)
+    {
+        Console.Write($"{PArray[j]} ");
+    }
+}
+
+
+int M = Prompt("введите число, равное желаемому количеству проверок на положительность числа: ");
+int number;
+int[]array = new int [M];
+
+for(int j = 0; j < M; j++)
+{
+    number = Prompt($"Введите число {1+j}");
+    array[j] = number;
+}
+Console.WriteLine($"Вы ввели");
+PrintArray(array);
+
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+        count++; 
+    }
+}
+Console.WriteLine($"Введено {count} положительных чисел");
---
+++ Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double Prompt (string message)
+{
+    Console.WriteLine(message);
+    double param = double.Parse(Console.ReadLine());
+    return param;
+}
+
+double k1 = Prompt("Введите k1 ");
+double b1 = Prompt("Введите b1 ");
+double k2 = Prompt("Введите k2 ");
+double b2 = Prompt("Введите b2 ");
+
+double x;
+double y;
+
+if (k1 == k2)
+{
+    if(b1 == b2)
+    {
+    Console.WriteLine($"Прямые совпадают");
+    }
+    else  Console.WriteLine($"Прямые параллельны");
+}
+else
+{
+x = (b2 - b1) / (k1 - k2);
+y = k2 * x + b2;  
+Console.WriteLine($"Прямые пересекаются в точке [{Math.Round(x, 1)}, {Math.Round(y, 1)}]");
+}
+
+
+
